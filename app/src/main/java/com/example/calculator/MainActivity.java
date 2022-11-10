c1db6915e002b87af3a3a81d6897a9398c7be328
Shafiujjaman-shafi <jcmshafi96@gmail.com>
2022-11-10 17:50:42 +0600

    small changes

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -25,43 +25,19 @@ public class MainActivity extends AppCompatActivity {
         outputTv = findViewById(R.id.outputTv);
 
 
-        btn0 = findViewById(R.id.btn0);
-        btn1 = findViewById(R.id.btn1);
-        btn2 = findViewById(R.id.btn2);
-        btn3 = findViewById(R.id.btn3);
-        btn4 = findViewById(R.id.btn4);
-        btn5 = findViewById(R.id.btn5);
-        btn6 = findViewById(R.id.btn6);
-        btn7 = findViewById(R.id.btn7);
-        btn8 = findViewById(R.id.btn8);
-        btn9 = findViewById(R.id.btn9);
 
         btnClear = findViewById(R.id.btnClear);
-        btnBack = findViewById(R.id.btnBack);
-
-        btnPlus = findViewById(R.id.btnPlus);
-        btnMinus = findViewById(R.id.btnMinus);
-        btnDivision = findViewById(R.id.btnDivision);
-        btnMultiply = findViewById(R.id.btnMultiply);
-
-        btnEqual = findViewById(R.id.btnEqual);
-
-        btnDot = findViewById(R.id.btnDot);
-
-        btnPercent = findViewById(R.id.btnPercent);
-
-        btnBracket = findViewById(R.id.btnBracket);
-
-
-
-
         btnClear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                inputTv.setText("");
-                outputTv.setText("");
+                tamp = inputTv.getText().toString();
+                if (tamp.length() != 0) {
+                    inputTv.setText("");
+                    outputTv.setText("");
+                }
             }
         });
+        btnBack = findViewById(R.id.btnBack);
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -74,13 +50,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
+
+
+
+
+
+
+
+        btn0 = findViewById(R.id.btn0);
         btn0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-             tamp = inputTv.getText().toString();
-             inputTv.setText(tamp + "0");
+                tamp = inputTv.getText().toString();
+                inputTv.setText(tamp + "0");
             }
         });
+        btn1 = findViewById(R.id.btn1);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -88,6 +75,7 @@ public class MainActivity extends AppCompatActivity {
                 inputTv.setText(tamp + "1");
             }
         });
+        btn2 = findViewById(R.id.btn2);
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -95,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
                 inputTv.setText(tamp + "2");
             }
         });
+        btn3 = findViewById(R.id.btn3);
         btn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -102,6 +91,7 @@ public class MainActivity extends AppCompatActivity {
                 inputTv.setText(tamp + "3");
             }
         });
+        btn4 = findViewById(R.id.btn4);
         btn4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -109,6 +99,7 @@ public class MainActivity extends AppCompatActivity {
                 inputTv.setText(tamp + "4");
             }
         });
+        btn5 = findViewById(R.id.btn5);
         btn5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -116,6 +107,7 @@ public class MainActivity extends AppCompatActivity {
                 inputTv.setText(tamp + "5");
             }
         });
+        btn6 = findViewById(R.id.btn6);
         btn6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -123,6 +115,7 @@ public class MainActivity extends AppCompatActivity {
                 inputTv.setText(tamp + "6");
             }
         });
+        btn7 = findViewById(R.id.btn7);
         btn7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -130,6 +123,7 @@ public class MainActivity extends AppCompatActivity {
                 inputTv.setText(tamp + "7");
             }
         });
+        btn8 = findViewById(R.id.btn8);
         btn8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -137,6 +131,7 @@ public class MainActivity extends AppCompatActivity {
                 inputTv.setText(tamp + "8");
             }
         });
+        btn9 = findViewById(R.id.btn9);
         btn9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -144,41 +139,7 @@ public class MainActivity extends AppCompatActivity {
                 inputTv.setText(tamp + "9");
             }
         });
-        btnPlus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tamp = inputTv.getText().toString();
-                inputTv.setText(tamp + "+");
-            }
-        });
-        btnMinus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tamp = inputTv.getText().toString();
-                inputTv.setText(tamp + "-");
-            }
-        });
-        btnMultiply.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tamp = inputTv.getText().toString();
-                inputTv.setText(tamp + "×");
-            }
-        });
-        btnDivision.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tamp = inputTv.getText().toString();
-                inputTv.setText(tamp + "÷");
-            }
-        });
-        btnPercent.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tamp = inputTv.getText().toString();
-                inputTv.setText(tamp + "%");
-            }
-        });
+        btnDot = findViewById(R.id.btnDot);
         btnDot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -186,6 +147,66 @@ public class MainActivity extends AppCompatActivity {
                 inputTv.setText(tamp + ".");
             }
         });
+        btnPercent = findViewById(R.id.btnPercent);
+        btnPercent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tamp = inputTv.getText().toString();
+                inputTv.setText(tamp + "%");
+            }
+        });
+
+
+
+
+
+
+
+
+
+        btnPlus = findViewById(R.id.btnPlus);
+        btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tamp = inputTv.getText().toString();
+                inputTv.setText(tamp + "+");
+            }
+        });
+        btnMinus = findViewById(R.id.btnMinus);
+        btnMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tamp = inputTv.getText().toString();
+                inputTv.setText(tamp + "-");
+            }
+        });
+        btnDivision = findViewById(R.id.btnDivision);
+        btnDivision.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tamp = inputTv.getText().toString();
+                inputTv.setText(tamp + "÷");
+            }
+        });
+        btnMultiply = findViewById(R.id.btnMultiply);
+        btnMultiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tamp = inputTv.getText().toString();
+                inputTv.setText(tamp + "×");
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+        btnBracket = findViewById(R.id.btnBracket);
         btnBracket.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -201,6 +222,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        btnEqual = findViewById(R.id.btnEqual);
         btnEqual.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -223,10 +245,6 @@ public class MainActivity extends AppCompatActivity {
                 outputTv.setText(finalResult);
             }
         });
-
-
-
-
 
 
 
